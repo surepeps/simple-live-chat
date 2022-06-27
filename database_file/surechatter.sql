5292a01fe76f995d8ed9afa207563959f91606d3
Hassan Tijani <gatukurh1@mail>
2022-06-28 00:54:37 +0100

    db update

--- database_file/surechatter.sql
+++ database_file/surechatter.sql
@@ -36,26 +36,6 @@ CREATE TABLE `sc_messages` (
   `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `sc_messages`
---
-
-INSERT INTO `sc_messages` (`msg_id`, `from_id`, `to_id`, `message`, `status`, `date_created`) VALUES
-(21, 19, 20, 'hello', 1, '2022-06-27 22:45:23'),
-(22, 20, 19, 'how you doing???', 1, '2022-06-27 22:45:40'),
-(23, 19, 20, 'hey', 1, '2022-06-27 22:47:45'),
-(24, 20, 19, 'i am here', 1, '2022-06-27 22:47:58'),
-(25, 19, 20, 'why is the design like that', 1, '2022-06-27 22:48:42'),
-(26, 20, 19, 'i dont know', 1, '2022-06-27 22:49:18'),
-(27, 20, 19, 'design@done', 1, '2022-06-27 23:01:33'),
-(28, 20, 21, 'hi', 1, '2022-06-27 23:02:31'),
-(29, 21, 20, 'how you doing', 1, '2022-06-27 23:02:47'),
-(30, 20, 21, 'i am doing good and you', 1, '2022-06-27 23:03:04'),
-(31, 19, 20, 'send to test time', 1, '2022-06-27 23:23:57'),
-(32, 19, 21, 'hey', 1, '2022-06-27 23:25:57'),
-(33, 21, 19, 'how you doing', 1, '2022-06-27 23:26:34'),
-(34, 19, 20, 'hey', 1, '2022-06-27 23:36:11');
-
 -- --------------------------------------------------------
 
 --
@@ -88,15 +68,6 @@ CREATE TABLE `sc_users` (
   `email_code` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `sc_users`
---
-
-INSERT INTO `sc_users` (`user_id`, `username`, `email`, `first_name`, `last_name`, `password`, `avartar`, `active`, `date_created`, `email_code`) VALUES
-(19, 'user_97366', 'gatukurh1@gmail.com', 'Hassan', 'Tijani', '$2y$10$Or4LlDzA5Af/VZtE7tKmg.x8nGYUmFb4TTKglkbETwyWASE9iFSAa', 'upload/photos/2022/06/1IFpBBL5UoTXgINSx28r_25_8abf21144dfa45be3b38a098ab9d6337_image.jpg', 1, '2022-06-25 19:55:24', '867bc95878ddc014e2eafab0081ad4b3'),
-(20, 'user_4684', 'gatukurh0@gmail.com', 'Surecoder', 'Gat', '$2y$10$tnTicBur6uJhaAGDLX7t9.7S11nYshE299tnE9mCS7qA.zEedqyaa', 'upload/photos/2022/06/EY5pErecBLLDswGfbBTQ_25_024b391d72aad16b487d6b994db640d9_image.jpg', 1, '2022-06-27 22:39:10', 'fc393ee7c96873c3badc2f6f50e02a6e'),
-(21, 'user_317212', 'fatimah@gmail.com', 'Fatimah', 'john', '$2y$10$EPXWyY4fO5RLDA8aoEr7/.XZx/rtK2PhBoCKG.3iCCjvGXxXNW2BW', 'upload/photos/2022/06/vjMKR34VTlB1vZfcLu9E_27_78bef7eb22a4a5f99fd6639a455a880b_image.jpg', 1, '2022-06-27 12:24:15', '8afd7d5fca542dd74918368245a52719');
-
 -- --------------------------------------------------------
 
 --
@@ -112,14 +83,6 @@ CREATE TABLE `sh_appssessions` (
   `time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `sh_appssessions`
---
-
-INSERT INTO `sh_appssessions` (`id`, `user_id`, `session_id`, `platform`, `platform_details`, `time`) VALUES
-(28, 20, 'caab9c99360a082cf4579cddcafc1e9564511e18ebf0c99b10d1ddf77e7ac66f09345d6a5048962427d52bcb3580724eb4cbe9f2718a9365', 'web', '{\"userAgent\":\"Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/102.0.5005.87 Mobile/15E148 Safari/604.1\",\"name\":\"Apple Safari\",\"version\":\"604.1\",\"platform\":\"mac\",\"pattern\":\"#(?<browser>Version|Safari|other)[/ ]+(?<version>[0-9.|a-zA-Z.]*)#\",\"ip_address\":\"::1\"}', 1656370460),
-(30, 19, 'd3ca74d1c08accd08f63bae06d7a8942777ff92f280891f1339fdcab632222dd00deb099461103602ea19e760aeeeeeb813a2406d0d31a25', 'web', '{\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36\",\"name\":\"Google Chrome\",\"version\":\"102.0.0.0\",\"platform\":\"windows\",\"pattern\":\"#(?<browser>Version|Chrome|other)[/ ]+(?<version>[0-9.|a-zA-Z.]*)#\",\"ip_address\":\"::1\"}', 1656370862),
-(31, 21, '61ee2a81b1e04f81019d5caaacc314a91a0b61beb5d39411285296bb41f4a71f2bafa80b1820463105e97c207235d63ceb1db43c60db7bbb', 'web', '{\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36\",\"name\":\"Google Chrome\",\"version\":\"102.0.0.0\",\"platform\":\"windows\",\"pattern\":\"#(?<browser>Version|Chrome|other)[/ ]+(?<version>[0-9.|a-zA-Z.]*)#\",\"ip_address\":\"::1\"}', 1656370922);
 
 -- --------------------------------------------------------
 
@@ -155,7 +118,7 @@ INSERT INTO `sh_config` (`id`, `name`, `value`) VALUES
 (15, 'siteEmail', ''),
 (16, 'defualtLang', 'english'),
 (17, 'emailValidation', '1'),
-(19, 'site_url', 'https://12ab-197-211-61-36.in.ngrok.io/surechatter'),
+(19, 'site_url', 'http://localhost/surechatter'),
 (20, 'prevent_system', '0'),
 (21, 'bad_login_limit', '23'),
 (22, 'lock_time', '23'),
@@ -217,36 +180,6 @@ INSERT INTO `sh_config` (`id`, `name`, `value`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `texster`
---
-
-CREATE TABLE `texster` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `avartar` text NOT NULL,
-  `email_code` text NOT NULL,
-  `active` int(11) NOT NULL DEFAULT 1,
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `texster`
---
-
-INSERT INTO `texster` (`user_id`, `username`, `first_name`, `last_name`, `email`, `password`, `avartar`, `email_code`, `active`, `date_created`) VALUES
-(1, 'jhjkl', '', '', '', '', '', '', 1, '2022-06-25 14:47:58'),
-(2, 'jhjkl', 'iuyghj', 'uytgfghj', '', '', '', '', 1, '2022-06-25 14:47:58'),
-(3, 'jhjkl', 'iuyghj', 'uytgfghj', 'hhagd@hhd.dcf', 'ghj', '', '', 1, '2022-06-25 14:47:58'),
-(4, 'jhjkl', 'iuyghj', 'uytgfghj', 'hhagd@hhd.dcf', 'ghj', '', 'hgfgvhjk', 1, '2022-06-25 14:47:58'),
-(5, 'jhjkl', 'iuyghj', 'uytgfghj', 'hhagd@hhd.dcf', 'ghj', '', 'hgfgvhjk', 1, '2022-06-25 14:47:58'),
-(6, 'jhjkl', 'iuyghj', 'uytgfghj', 'hhagd@hhd.dcf', 'ghj', '', 'hgfgvhjk', 1, '2022-06-25 14:47:59'),
-(7, 'jhjkl', 'iuyghj', 'uytgfghj', 'hhagd@hhd.dcf', 'ghj', '', 'hgfgvhjk', 1, '2022-06-25 14:48:47');
-
---
 -- Indexes for dumped tables
 --
 
@@ -280,11 +213,6 @@ ALTER TABLE `sh_appssessions`
 ALTER TABLE `sh_config`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `texster`
---
-ALTER TABLE `texster`
-  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -320,12 +248,6 @@ ALTER TABLE `sh_appssessions`
 ALTER TABLE `sh_config`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
---
--- AUTO_INCREMENT for table `texster`
---
-ALTER TABLE `texster`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
